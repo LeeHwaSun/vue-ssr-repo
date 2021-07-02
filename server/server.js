9c2de864d929658b70f2bc4b968f2594e9290e1f
LeeHwaSun <op_instinct@naver.com>
2021-07-02 20:58:44 +0900

    소스 수정

--- server/server.js
+++ server/server.js
@@ -19,23 +19,23 @@ const serverBundle = require(path.join(__dirname, '../dist/vue-ssr-server-bundle
 const clientManifest = require(path.join(__dirname, '../dist/vue-ssr-client-manifest.json'));
 
 const renderer = createBundleRenderer(serverBundle, {
-    runInNewContext : false,
-    template,
-    clientManifest,
+	runInNewContext : false,
+	template,
+	clientManifest,
 });
 
 app.get('*', (req, res) => {
-    const ctx = {
-        url : req.url,
-        title : 'Vue SSR App',
-        metas : `<!-- inject more metas -->`,
-    };
+	const ctx = {
+		url : req.url,
+		title : 'Vue SSR App',
+		metas : `<!-- inject more metas -->`,
+	};
 
-    const stream = renderer.renderToStream(ctx);
+	const stream = renderer.renderToStream(ctx);
 
-    stream.on('end', () => {
-        console.log('스트림 렌더 종료');
-    }).pipe(res);
+	stream.on('end', ()=> {
+		console.log('스트림 렌더 종료');
+	}).pipe(res);
 });
 
 // 서버 Listen
