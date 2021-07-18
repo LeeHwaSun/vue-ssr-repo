114c148c5fac00b69d544b57dfa992db998a5d97
LeeHwaSun <op_instinct@naver.com>
2021-07-18 19:53:50 +0900

    Menu 1차 작업

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <v-app>
+    <v-navigation-drawer app v-model="drawer" :width="drawerWidth">
+      <site-navi @close="toggleDrawer"/>
+    </v-navigation-drawer>
+
     <v-app-bar app color="primary" dark hide-on-scroll>
+      <v-app-bar-nav-icon @click="toggleDrawer" />
       <site-header></site-header>
       <v-spacer></v-spacer>
     </v-app-bar>
@@ -16,12 +21,25 @@
 <script>
 import SiteHeader from './components/layout/SiteHeader.vue';
 import SiteFooter from './components/layout/SiteFooter.vue';
+import SiteNavi from './components/layout/SiteNavi.vue';
 export default {
-  components: { SiteHeader, SiteFooter },
+  components: { SiteHeader, SiteFooter, SiteNavi },
   name: 'App',
 
-  data: () => ({
-    //
-  }),
+  data() {
+    return {
+      drawer : false,
+    }
+  },
+  computed: {
+    drawerWidth() {
+      return this.$vuetify.breakpoint.xs ? '100%' : '360';
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.drawer = !this.drawer;
+    }
+  }
 };
 </script>
